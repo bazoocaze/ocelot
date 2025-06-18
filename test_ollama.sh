2d6ed678143663f7924e31f49a8377d45376363a
Jasf <bazoocaze@gmail.com>
2025-06-17 21:59:10 -0300

    chore: remove test_ollama.sh and test_openrouter.sh

--- test_ollama.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-./ocelot.sh "qwen3:4b" "Who wrote Harry Potter?"
-
--- test_openrouter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-./ocelot.sh --debug "openrouter/meta-llama/llama-3.3-8b-instruct:free" "Who wrote Harry Potter?"
-
